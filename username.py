6b7e5ebd0782e0c87ad4e9e43033fc7cc418ffee
ikahih.cyris <ikahih.cyris@awatapu.school.nz>
2022-05-09 09:55:50 +1200

    out with the old in with the new

--- username.py
+++ username.py
@@ -7,24 +7,56 @@ import random
 letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
           'w', 'x', 'y', 'z']
 
-# bunch of list names
+# bunch of lists that will keep track of a "student's" first name, surname and year level
 
-first_names = []
-surnames = []
-year_level = []
+first_names = surnames = year_level = []
+not_finished = True
 
 # this first for loop should repeat it between 100 to 200 times, the second and third for loop should randomise the
 # student's name and the random.randint should randomise the student's year level
+
 
 for student in range(random.randint(50, 100)):
     first_names.append("")
     surnames.append("")
     for i in range(random.randint(3, 20)):
-        first_names[student] = first_names[student] + random.choice(letter)
+        first_names[student] = str(first_names[student]) + random.choice(letter)
     for i in range(random.randint(3, 20)):
         surnames[student] = surnames[student] + random.choice(letter)
     year_level.append(random.randint(9, 13))
 
-print(first_names)
-print(surnames)
-print(year_level)
+
+# question_function that will reiterate itself until the user has input a valid value. If the user inputs an invalid
+# value the function should display the error_message parameter
+
+
+def question_func(question, error_message):
+    while True:
+        print()
+        answer = input(question).lower().strip()
+        if len(answer) == 0:
+            text("Error, " + error_message)
+        else:
+            return answer
+
+
+# text function, not for any other purpose than aesthetics
+
+
+def text(message):
+    print()
+    print(message)
+
+
+# main program running
+
+
+while not_finished:
+    response = question_func("Enter a student's username (enter ? for help)", "enter a student's username")
+    if response == "?":
+        response = question_func("Do you need to see their usernames?", "enter either y/n")
+        if response == "y":
+            for student in first_names:
+                print(str(year_level[first_names.index(student)]) + surnames[first_names.index(student)].title()
+                      + student[0].upper())
+
